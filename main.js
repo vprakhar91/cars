e4f8207c29eb4895bd5bdeaa4c23c2af9ded15cf
vprakhar91 <46259033+vprakhar91@users.noreply.github.com>
2022-03-14 09:56:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+let menu= document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+   menu.classList.toggle('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
